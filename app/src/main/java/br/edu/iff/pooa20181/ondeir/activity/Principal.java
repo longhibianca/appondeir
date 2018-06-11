8475cdac6040bc882706fa1aec06c98d36b5b47c
Luiz Gustavo <lglmoura@gmail.com>
2018-06-11 18:00:58 -0300

    Refatorando o Layout do Activy Principal

--- app/src/main/java/br/edu/iff/pooa20181/ondeir/activity/Principal.java
+++ app/src/main/java/br/edu/iff/pooa20181/ondeir/activity/Principal.java
@@ -1,15 +1,46 @@
 package br.edu.iff.pooa20181.ondeir.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import br.edu.iff.pooa20181.ondeir.R;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    private String[] activities = {"ListaEventoActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activities);
+
+        ListView listView = (ListView) findViewById(R.id.listaMenu);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+        Intent intent = null;
+
+        try {
+
+            Class obj = Class.forName ("br.edu.iff.pooa20181.ondeir.activity."+activities[position]);
+            intent = new Intent(this, obj);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(intent);
+
+    }
+
 }
