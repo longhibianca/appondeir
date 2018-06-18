5fe1c7d9642973783af8fdf3930337b31abe9f2d
Luiz Gustavo <lglmoura@gmail.com>
2018-06-18 13:12:07 -0300

    Criando arquivo de Configuração do realm

---
+++ app/src/main/java/br/edu/iff/pooa20181/ondeir/model/BDConfig.java
@@ -0,0 +1,22 @@
+package br.edu.iff.pooa20181.ondeir.model;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class BDConfig extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(getApplicationContext());
+
+        RealmConfiguration.Builder builder = new RealmConfiguration.Builder();
+        builder.name("evento.realm");
+        builder.schemaVersion(0);
+        builder.deleteRealmIfMigrationNeeded();
+        RealmConfiguration realmConfiguration = builder.build();
+        Realm.setDefaultConfiguration(realmConfiguration);
+    }
+}
