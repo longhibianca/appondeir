bf0d4983525ee10b0730b30d699b8137d83ca6ac
Luiz Gustavo <lglmoura@gmail.com>
2018-06-18 11:17:59 -0300

    Refatorando a Classe Evento

--- app/src/main/java/br/edu/iff/pooa20181/ondeir/model/Evento.java
+++ app/src/main/java/br/edu/iff/pooa20181/ondeir/model/Evento.java
@@ -6,17 +6,29 @@ public class Evento {
 
     private int id;
     private String nome;
-    private String endereco;
+    private String rua;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private float longitude;
+    private float latitude;
     private Date data;
+    private int capacidade;
 
     public Evento() {
     }
 
-    public Evento(int id, String nome, String endereco, Date data) {
+    public Evento(int id, String nome, String rua, String numero, String bairro, String cidade, float longitude, float latitude, Date data, int capacidade) {
         this.id = id;
         this.nome = nome;
-        this.endereco = endereco;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.longitude = longitude;
+        this.latitude = latitude;
         this.data = data;
+        this.capacidade = capacidade;
     }
 
     public int getId() {
@@ -35,12 +47,52 @@ public class Evento {
         this.nome = nome;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public String getRua() {
+        return rua;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
     }
 
     public Date getData() {
@@ -49,5 +101,13 @@ public class Evento {
 
     public void setData(Date data) {
         this.data = data;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
     }
 }
